b853d5b8a7040c6bf9c897efc313cb4ac27b3f7d
Sharan-singalCODE <68741182+Sharan-singalCODE@users.noreply.github.com>
2022-01-20 17:41:06 +0530

    Add files via upload

---
+++ screens/LoadingScreen.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import firebase from 'firebase';
+export default class Loading extends React.Component{
+  checkIfLoggedIn=()=>{
+    firebase.auth().onAuthStateChanged((user) => {
+      if(user){
+        this.props.navigation.navigate('Home')
+
+      }
+      else{
+        this.props.navigation.navigate('Login')
+      }
+    })
+  }
+  render(){
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Loading..</Text>
+      </View>
+    )
+  }
+
+  }
